76a35f656f333a013ba02d197f739500f5d430e1
Kenneth Kangethe <kangethethuku@gmail.com>
2026-06-03 22:17:28 +0300

    Add app routes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,21 @@
-import './App.css'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import DeviceListPage from "./pages/DeviceListPage";
+import DeviceDetailPage from "./pages/DeviceDetailPage";
+import RegisterDevicePage from "./pages/RegisterDevicePage";
 
 function App() {
   return (
-    <>
-      Hello World
-    </>
-  )
+    <BrowserRouter>
+      <div className="min-h-screen bg-background text-foreground">
+        <Routes>
+          <Route path="/" element={<DeviceListPage />} />
+          <Route path="/devices" element={<DeviceListPage />} />
+          <Route path="/devices/register" element={<RegisterDevicePage />} />
+          <Route path="/devices/:id" element={<DeviceDetailPage />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
